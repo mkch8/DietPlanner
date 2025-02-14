a66304cb1b3cc8cdf68889e9a2a8045d83af8f94
Michael <michael.kch8@gmail.com>
2025-02-14 20:33:03 +0000

    Added input form and button to meal generator

--- frontend/src/components/MealGenerator.jsx
+++ frontend/src/components/MealGenerator.jsx
@@ -1,15 +1,25 @@
-const MealGenerator = () => {
-    const [calories, setCalories] = useState ({
-        calories: '',
-    });
+import React, { useEffect, useState } from 'react';
 
-    const fetchMealPlan = (mpCalories) => {
+const MealGenerator = () => {
+    const [calories, setCalories] = useState ('');
+
+    const fetchMealPlan = (calories) => {
         
     }
 
     return (
         <div className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-md">
             <h1>Meal Generator</h1>
+            <input
+                type = "number"
+                name = "calories"
+                placeholder = "Calories (kcal)"
+                className="border border-gray-300 p-2 rounded w-full"
+                onChange={(event) => setCalories(event.target.value)}
+            />
+            <button onClick={fetchMealPlan} className="bg-green-500 text-white p-3 rounded w-full">
+                Generate Meal Plan
+            </button>
         </div>
     );
 };
